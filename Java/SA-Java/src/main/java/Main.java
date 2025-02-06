69146baa563249743d40c0c471739ec1b893531d
Giuseppe Mattia Greco <giumatt99@gmail.com>
2025-02-06 12:49:29 +0100

    Added model selection menu and created config.properties file with model paths. Added language storing option to DB.

--- Java/SA-Java/src/main/java/Main.java
+++ Java/SA-Java/src/main/java/Main.java
@@ -8,19 +8,41 @@ import java.nio.charset.StandardCharsets;
 import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class Main {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     private static final int PAUSE_THRESHOLD = 2000;
     private static final int MIN_WORDS_THRESHOLD = 5;
-    private static final String NODE_RED_URL = "http://localhost:1880/sentiment";
+    private static String NODE_RED_URL;
 
     public static void main(String[] args) throws LineUnavailableException, IOException {
+        Properties properties = loadConfig("config.properties");
+
+        String modelPathIT = properties.getProperty("vosk.model.it");
+        String modelPathEN = properties.getProperty("vosk.model.en");
+        NODE_RED_URL = properties.getProperty("node_red_url");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select the language:");
+        System.out.println("1 - Italian");
+        System.out.println("2 - English");
+        System.out.println("Choice: ");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        String language = (choice == 1) ? "Italian" : "English";
+        String MODEL_PATH = language.equals("Italian") ? modelPathIT : modelPathEN;
+
+        System.out.println("Running instance with " + language + " model.");
+
         AudioCapture audio = new AudioCapture();
-        SpeechToText speech = new SpeechToText("/home/mattia/Documents/vosk-model-en-us-0.22");
+        SpeechToText speech = new SpeechToText(MODEL_PATH);
         SentimentAnalysis sentiment = new SentimentAnalysis();
-        DataStorage storage = new DataStorage();
 
         // Part for defining sentence length and pauses between sentences
         StringBuilder phrase = new StringBuilder();
@@ -49,7 +71,7 @@ public class Main {
                         String sentimentLabel = sentiment.analyzeSentiment(phrase.toString().trim());
 
                         //storage.saveSentence(phrase.toString().trim(), sentimentLabel);
-                        sendToNodeRedAsync(phrase.toString().trim(), sentimentLabel);
+                        sendToNodeRedAsync(phrase.toString().trim(), sentimentLabel, language);
 
                         //System.out.println("Saved: " + phrase + "with sentiment: " + sentimentLabel);
                         LOGGER.info("Sent to Node-RED: " + phrase + " | Sentiment: " + sentimentLabel);
@@ -62,7 +84,7 @@ public class Main {
                     String sentimentLabel = sentiment.analyzeSentiment(phrase.toString().trim());
 
                     //storage.saveSentence(phrase.toString().trim(), sentimentLabel);
-                    sendToNodeRedAsync(phrase.toString().trim(), sentimentLabel);
+                    sendToNodeRedAsync(phrase.toString().trim(), sentimentLabel, language);
                     //System.out.println("Saved: " + phrase + "with sentiment: " + sentimentLabel);
                     LOGGER.info("Sent to Node-RED: " + phrase + " | Sentiment: " + sentimentLabel);
                     phrase.setLength(0);
@@ -72,13 +94,25 @@ public class Main {
                     Thread.sleep(50);
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
-                    System.err.println("Interrupped thread: " + e.getMessage());
+                    System.err.println("Interrupted thread: " + e.getMessage());
                 }
             }
         }
     }
 
-    private static void sendToNodeRed(String text, String sentiment) {
+    private static Properties loadConfig(String path) {
+        Properties properties = new Properties();
+
+        try (FileInputStream fis = new FileInputStream(path)) {
+            properties.load(fis);
+        } catch (IOException e) {
+            System.err.println("Error while loading configuration: " + e);
+        }
+
+        return properties;
+    }
+
+    private static void sendToNodeRed(String text, String sentiment, String language) {
         HttpURLConnection conn = null;
         try {
             URI uri = new URI(NODE_RED_URL);
@@ -88,7 +122,7 @@ public class Main {
             conn.setRequestProperty("Content-Type", "application/json");
             conn.setDoOutput(true);
 
-            String jsonInputString = "{\"text\": \"" + text + "\", \"sentiment\": \"" + sentiment + "\"}";
+            String jsonInputString = "{\"text\": \"" + text + "\", \"sentiment\": \"" + sentiment + "\", \"language\": \"" + language + "\"}";
             try (OutputStream os = conn.getOutputStream()) {
                 byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
                 os.write(input, 0, input.length);
@@ -107,7 +141,7 @@ public class Main {
         }
     }
 
-    private static void sendToNodeRedAsync(String text, String sentiment) {
-        new Thread(() -> sendToNodeRed(text, sentiment)).start();
+    private static void sendToNodeRedAsync(String text, String sentiment, String language) {
+        new Thread(() -> sendToNodeRed(text, sentiment, language)).start();
     }
 }
